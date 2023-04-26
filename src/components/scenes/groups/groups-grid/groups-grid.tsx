6bfe03466937e4d3dcbcdcfa9c384f51186ff66b
bhagothamgk <gopikrishna@kireetisoft.com>
2023-04-27 01:43:58 +0530

    added,custom hook

--- src/components/scenes/groups/groups-grid/groups-grid.tsx
+++ src/components/scenes/groups/groups-grid/groups-grid.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Col, Modal, Row, Table } from "react-bootstrap";
+import { Button, Card, Col, Modal, Row, Table } from "react-bootstrap";
 import { CardBody, CardHeader } from "reactstrap";
 import CreateGroup from "../forms/create-group";
 import axios from "axios";
@@ -11,9 +11,9 @@ import UrlConstants from "../../../constants/UrlConstants";
 
 export default function GroupsGrid() {
   const { GET_ALL_CHIT_PLANS } = UrlConstants();
-  const { response, loading } = useFetch(GET_ALL_CHIT_PLANS, 'GET');
+  const { response, loading, onRefresh: GetChits } = useFetch({ url: GET_ALL_CHIT_PLANS, Options: { method: "GET",initialRender:true } });
 
-  return (<>
+  return (<><Button onClick={() => GetChits()}>load on triggger</Button>
     <Grid data={response} as={CustomRow} loading={loading}>
       <GridColumn title="Group Name" targetField="groupName" />
       <GridColumn title="Amount" targetField="amount" />

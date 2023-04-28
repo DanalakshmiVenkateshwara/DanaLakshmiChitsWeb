3664adc62bfcd756efc025b34329e8d119f7dcba
bhagothamgk <gopikrishna@kireetisoft.com>
2023-04-28 20:05:59 +0530

    res

--- src/components/shared/sidebar/SideBarItem.tsx
+++ src/components/shared/sidebar/SideBarItem.tsx
@@ -6,14 +6,14 @@ interface Iprops {
   label: string;
   icon: JSX.Element;
   path: string;
+  isClose?: boolean;
 }
 export default function SideBarItem(props: Iprops) {
-  const [isClose, setIsClose] = useState(true);
   return (
     <NavLink to={props.path} className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "") + " sideBarItem d-block px-2 py-3 border-bottom border-secondary text-decoration-none"}>
       <Row className="text-white mx-0 align-items-center">
         <div style={{ width: "40px" }}>{props.icon}</div>
-        {!isClose && <Col >{props.label}</Col>}
+        {!props.isClose && <Col >{props.label}</Col>}
       </Row>
     </NavLink>
   );

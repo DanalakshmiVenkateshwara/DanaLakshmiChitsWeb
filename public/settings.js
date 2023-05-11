f4a1ba7c08a6945f2874b57244dd35bf37bc80f7
abbuanvesh <abbuanvesh@gmail.com>
2023-05-11 08:19:45 +0530

    url changes

--- public/settings.js
+++ public/settings.js
@@ -1,2 +1,2 @@
-window.gbl_React_App_Service_URL = "http://localhost:4400/";
+window.gbl_React_App_Service_URL = "http://127.0.0.1:5000/"; //"http://localhost:4400/";
 window.gbl_React_App_URL = "http://localhost:3000/";

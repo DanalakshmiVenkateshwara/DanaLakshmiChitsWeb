481e067523c64ffd4e48a49918e360e958aee2fa
DanalakshmiVenkateshwara <danalakshmichits@gmail.com>
2023-06-23 22:51:16 +0530

    auction changes

--- src/components/scenes/auctionDetails/customrow.tsx
+++ src/components/scenes/auctionDetails/customrow.tsx
@@ -9,12 +9,12 @@ export default function CustomRow(props:any){
     return (
     <>
        {/* <GridCell title="UserName" targetField="userName"><>{data?.userName}</></GridCell> */}
-      <GridCell title="UserName" targetField="userName">{data.userName}</GridCell>
-      <GridCell title="GroupName" targetField="groupName">{data.groupName}</GridCell>
-      <GridCell title="AuctionDate" targetField="auctionDate">{data.auctionDate}</GridCell>
-      <GridCell title="AuctionMonth" targetField="paidUpto">{data.paidUpto}</GridCell>
-      <GridCell title="Amount" targetField="amount">{data.amount}</GridCell>
-      <GridCell title="Dividend" targetField="dividend">{data.dividend}</GridCell>
+      <GridCell title="UserName" targetField="userName">{data?.userName}</GridCell>
+      <GridCell title="GroupName" targetField="groupName">{data?.groupName}</GridCell>
+      <GridCell title="AuctionDate" targetField="auctionDate">{data?.auctionDate}</GridCell>
+      <GridCell title="AuctionMonth" targetField="paidUpto">{data?.paidUpto}</GridCell>
+      <GridCell title="Amount" targetField="amount">{data?.amount}</GridCell>
+      <GridCell title="Dividend" targetField="dividend">{data?.dividend}</GridCell>
       <GridCell title="" targetField="">
         <Button variant='primary' size='sm' onClick={()=>navigate('/participate')}>Participate</Button>
       </GridCell>

6fc5a18a0aa8288b8f7706156d4b52ddda8e78d0
DanalakshmiVenkateshwara <anvesh.abbu143@gmail.com>
2023-07-15 15:49:09 +0530

    user profile and contact us  changes

--- src/components/scenes/userprofile/userprofile.tsx
+++ src/components/scenes/userprofile/userprofile.tsx
@@ -1,47 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import useFetch from '../../hooks/useFetch';
 import Form from '../../shared/form';
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Form as RForm } from 'react-bootstrap';
 import useNoninitialEffect from '../../hooks/useNoninitialEffect';
+import Card from '../../shared/card';
 export default function UserProfile() {
     const [userDetails, setUserDetails] = useState<any>({name: '', phone: '', eMail: '',aadhar: '', address: ''});
     const { response: usersResponse, loading: usersLoading } = useFetch({ url: `/Admin/GetUsers/${2}/${true}`, Options: { method: 'GET', initialRender: true } });
   
     useEffect(() => {
-        
+        debugger
         let data: any = usersResponse;
         
         if(data != undefined){
             
-        setUserDetails({...userDetails, name:data[0]?.name})
-        setUserDetails({...userDetails, phone:data[0]?.phone})
-        setUserDetails({...userDetails, eMail:data[0]?.eMail})
-        setUserDetails({...userDetails, aadhar:data[0]?.aadhar})
-        setUserDetails({...userDetails, address:data[0]?.address})
+        setUserDetails({...userDetails, name:data[0]?.name,phone:data[0]?.phone,eMail:data[0]?.eMail})
+        // setUserDetails({...userDetails, phone:data[0]?.phone})
+        // setUserDetails({...userDetails, eMail:data[0]?.eMail})
+        // setUserDetails({...userDetails, aadhar:data[0]?.aadhar})
+        // setUserDetails({...userDetails, address:data[0]?.address})
     }
       }, [usersResponse])
     return (
    <>
-    {/* <Form>
+    <Card>
             <Row className="mx-0" >
+                <Row>
                 <Col xl="3" lg="4" md="6">
-                    <Form.Label>Name:/</Form.Label> <Form.Label>{userDetails.name}</Form.Label>
-                </Col>
-                <Col xl="3" lg="4" md="6">
-                    <Form.Label>Phone:</Form.Label> <Form.Label>{userDetails.phone}</Form.Label>
-                </Col>
-                <Col xl="3" lg="4" md="6">
-                    <Form.Label>Email:</Form.Label> <Form.Label>{userDetails.eMail}</Form.Label>
-                </Col>
-                <Col xl="3" lg="4" md="6">
+                    <RForm.Label style={{fontWeight:'bold'}}>Name:</RForm.Label> <RForm.Label>{userDetails.name}</RForm.Label>
+                </Col></Row>
+                <Row><Col xl="3" lg="4" md="6">
+                    <RForm.Label style={{fontWeight:'bold'}}>Phone:</RForm.Label> <RForm.Label>{userDetails.phone}</RForm.Label>
+                </Col></Row>
+                <Row><Col xl="3" lg="4" md="6">
+                    <RForm.Label style={{fontWeight:'bold'}}>Email:</RForm.Label> <RForm.Label>{userDetails.eMail}</RForm.Label>
+                </Col></Row>
+                {/* <Col xl="3" lg="4" md="6">
                     <Form.Label>Aadhar:</Form.Label> <Form.Label>{userDetails.aadhar}</Form.Label>
                 </Col>
                 <Col xl="3" lg="4" md="6">
                     <Form.Label>Address:</Form.Label> <Form.Label>{userDetails.address}</Form.Label>
-                </Col>
-                <Form />
+                </Col> */}
+                {/* <Form /> */}
             </Row>
-        </Form> */}
+        </Card>
    </>
   )
 }
